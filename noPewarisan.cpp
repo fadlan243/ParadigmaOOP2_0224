c1db2185e9a4744d786ea3e86e6346cba64b2b3a
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-02 18:45:59 +0700

    add library

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,25 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class baseClass
-{
-public:
-    virtual void perkenalan()
-    {
-        cout << "Hallo saya function dari base Class";
-    }
-};
-
-class derivedclass : public baseClass
-{
-public:
-    void perkenalan()
-    {
-        cout << "Hallo saya function dari derived Class";
-    }
-}
-
-int main()
-{
-    derivedClass
-}
