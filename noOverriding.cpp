467c9547e6c89ffc0ef5e12d6997911c3302227b
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-02 18:48:31 +0700

    public baseClass

---
+++ noOverriding.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class baseClass
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya function dari base Class";
+    }
+};
+
+class derivedclass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya function dari derived Class";
+    }
+};
+
+int main()
+{
+    derivedclass a;
+    a.perkenalan();
+
+    return 0;
+}
