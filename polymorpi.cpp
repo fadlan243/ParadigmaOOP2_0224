fe8a6fbcf0135eed76b8ca9322c408def2bca5ef
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-02 17:23:34 +0700

    class seseorang

---
+++ polymorpi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class seseorang
+{
+public:
+    // Virtual void pesan = 0;
+    virtual void pesan()
+    {
+        cout << "Pesan dari seseorang" << endl;
+    }
+};
