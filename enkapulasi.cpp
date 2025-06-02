c12e63d0595c0d359eacd0468e2b5bb58bd48e18
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-02 18:39:14 +0700

    enkapulasi

--- enkapulasi.cpp
+++ enkapulasi.cpp
@@ -11,4 +11,25 @@ public:
     {
         saklarNo[i] value;
     }
+    string getSaklarNo(int i)
+    {
+        return saklarNo[i];
+    }
+};
+
+int main()
+{
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(0, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(0, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(0, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
     
+    return 0;
+};
