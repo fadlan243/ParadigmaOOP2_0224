49fb92f3b03a8143fac70b720e6f699ed2bd46be
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-02 18:15:41 +0700

    add library

--- polymorpi.cpp
+++ polymorpi.cpp
@@ -1,33 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class seseorang
-{
-public:
-    // Virtual void pesan = 0;
-    virtual void pesan()
-    {
-        cout << "Pesan dari seseorang" << endl;
-    }
-
-};
-
-class joko : public seseorang
-{
-public:
-    void pesan()
-    {
-        cout << "Pesan dari Joko" << endl;
-    }
-};
-
-class lia : public seseorang
-{
-public:
-    void pesan()
-    {
-        cout << "Pesan dari lia" << endl;
-    }
-};
-
-int main()
