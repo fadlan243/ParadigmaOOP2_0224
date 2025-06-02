834ca3981b50e4d3a45bcdb1319053c9e165e072
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-06-02 18:50:28 +0700

    void perkenalan

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -4,4 +4,18 @@ using namespace std;
 class baseClass
 {
 public:
-    
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya function dari base Class";
+    }
+};
+
+class derivedclass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya function dari derived Class";
+    }
+};
+
